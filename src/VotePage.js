fe09f310861abf482a661a63d1a6b98a1950d045
dreamylife1988 <peyman.nasri@yahoo.com>
2022-11-21 13:38:41 +0100

    tried to see how to get the random user's name

--- src/VotePage.js
+++ src/VotePage.js
@@ -1,25 +1,27 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 const VotePage = () => {
-const GetRandomUser = async () => {
+  const GetRandomUser = async () => {
     const { data: users } = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
+      "https://jsonplaceholder.typicode.com/users"
     );
-  
+
     const randomUser = users[Math.floor(Math.random() * users.length)];
     const userName = randomUser.name;
     console.log(userName);
-}
-  
-    return (
-      <div>
-        <h2>Welcome </h2>
-        <button onClick={GetRandomUser}>Draw Name</button>
-        <h3>You are a secret sender for</h3>
-        <p></p>
-      </div>
-    );
+    
+    // const userName = Object.keys(randomUser).map((name) => randomUser[name])
+    // console.log(users.map(({ name }) => name));
+  };
+
+  return (
+    <div>
+      <h2>Welcome </h2>
+      <button onClick={GetRandomUser}>Draw Name</button>
+      <h3>You are a secret sender for</h3>
+      <p></p>
+    </div>
+  );
 };
-  
-export default VotePage
+
+export default VotePage;
