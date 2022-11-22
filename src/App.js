440ae12a060c8c69eb43e171b4a12657d4ba95be
dreamylife1988 <peyman.nasri@yahoo.com>
2022-11-22 13:07:00 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import VotePage from "./VotePage";
 
@@ -5,10 +6,20 @@ function App() {
   return (
     <>
       <h1>Welcome to Soltia</h1>
-      <button>Become a Secret Sender</button>
-      <VotePage />
+
+      <Routes>
+        <Route exact path="/" element={<StartGame />} />
+        <Route path="/votepage" element={<VotePage />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
     </>
   );
 }
 
 export default App;
+
+const StartGame = () => (
+  <Link to={`/votepage`}>
+    <button>Become a Secret Sender</button>
+  </Link>
+);
